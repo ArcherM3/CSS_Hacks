fb3bda18b5daf6a99e68f73bd318f14ace2ebcb0
Nadeem-ui247 <nadeemdu2006@gmail.com>
2026-04-16 11:46:54 +0530

    Enhanced admin signup
    Added localStorage functionality and redirection after successful admin registration.

--- admin-signup.js
+++ admin-signup.js
@@ -10,13 +10,27 @@ document.getElementById("adminForm").addEventListener("submit", function(e) {
 
     // Basic validation
     if (name && email && password && department) {
-        message.style.color = "green";
-        message.innerText = "Admin registered successfully!";
-        
-        // Example: reset form
-        document.getElementById("adminForm").reset();
-    } else {
+    message.style.color = "green";
+    message.innerText = "Admin registered successfully!";
+    
+    localStorage.setItem("loggedIn", "true");
+
+    window.location.href = "home_page.html";
+
+    document.getElementById("adminForm").reset();
+}
+     else {
         message.style.color = "red";
         message.innerText = "Please fill all fields!";
     }
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    localStorage.setItem("loggedIn", "true");
+
+    window.location.href = "home_page.html";
 });
